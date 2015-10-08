eeb219d9227d2ca7cb5818d74bbad5d3bc02efc3
andela-abankole <akinjide.bankole@andela.co>
2015-10-08 12:16:29 +0100

    chore (bash): configure bash file

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# install dependencies
+echo "Hi, $USER"
+echo "*************************"
+echo "installing dependencies ..."
+echo "*************************"
+npm install
+
+# start server 
+echo "serving on port 3000 ...." 
+echo "*************************"
+
+# open browser
+echo "Please open your browser and go to localhost:3000 ....."
+npm start
