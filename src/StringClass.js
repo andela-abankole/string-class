883bf82585cf8b5f27e226e9da81b244ab453481
andela-abankole <akinjide.bankole@andela.co>
2015-10-12 12:34:06 +0100

    feat (toUpper): add toUpper method

--- src/StringClass.js
+++ src/StringClass.js
@@ -12,3 +12,25 @@ String.prototype.hasVowels = function() {
 
   return patt.test(_this) ? true : false;
 };
+
+/**
+ * [toUpper find any lowercase character between the brackets]
+ * @return {[String]}  [Returns all characters in uppercase]
+ */
+String.prototype.toUpper = function() {
+  var patt  = /[a-z]/gi,
+      _this = this,
+      str;
+  /**
+   * [upper matches the unicode character and subtracts 32]
+   * @param  {[String]} txt [contain regex solution]
+   * @return {[String]}     [convert `str` value to characters]
+   */
+  function upper(txt) {
+    str = txt.charCodeAt(0) - 32;
+    return String.fromCharCode(str);
+  }
+
+  return _this.replace(patt, upper);
+};
+
